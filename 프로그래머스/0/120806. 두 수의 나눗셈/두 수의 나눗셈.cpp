aeaffe6eb10b552ab56976c1a4beb02d41b30ce8
PDY <164182578+pdy127001@users.noreply.github.com>
2025-08-18 09:15:31 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 3.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    double result=(double)num1/(double)num2;
+    result*=1000;
+    answer=(int)result;
+    return answer;
+}
