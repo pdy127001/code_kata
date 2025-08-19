f9d35b10e71da4c022851a16cb4fc964e467a535
PDY <164182578+pdy127001@users.noreply.github.com>
2025-08-19 09:15:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 3.86 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = -1;
+    for(long i=0;i*i<=n;i++){
+        if(i*i==n){
+            answer=(i+1)*(i+1);
+        }
+    }
+        
+    
+    return answer;
+}
