80d87b74337d435dd9912aae9e62f6824a37b24d
PDY <164182578+pdy127001@users.noreply.github.com>
2025-08-28 09:08:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <cctype>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int idx = 0; 
+
+    for (char c : s) {
+        if (c == ' ') { 
+            answer += c;   
+            idx = 0;      
+        } else {
+            if (idx % 2 == 0) answer += toupper(c);
+            else answer += tolower(c);
+            idx++;
+        }
+    }
+
+    return answer;
+}
