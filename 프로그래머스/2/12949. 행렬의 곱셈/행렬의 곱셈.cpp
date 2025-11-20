1ce036f7f9723cfd6a6e16c733bb5bb3b5c37f1c
PDY <164182578+pdy127001@users.noreply.github.com>
2025-11-20 10:53:37 +0900

    Refactor matrix multiplication logic for clarity

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -4,17 +4,12 @@
 using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
-    int n = arr1.size();           // arr1 행
-    int m = arr1[0].size();        // arr1 열 = arr2 행
-    int p = arr2[0].size();        // arr2 열
-
-    vector<vector<int>> answer(n, vector<int>(p, 0));
-
-    for (int i = 0; i < n; i++) {        // arr1 행
-        for (int j = 0; j < p; j++) {    // arr2 열
-            for (int k = 0; k < m; k++) { // arr1의 열 = arr2의 행
-                answer[i][j] += arr1[i][k] * arr2[k][j];
-            }
+    vector<vector<int>> answer(arr1.size(),vector<int>(arr2[0].size(),0));
+    for(int i=0;i<arr1.size();i++){
+        for(int j=0;j<arr2[0].size();j++){
+             for (int k=0; k<arr1[0].size();k++){
+                answer[i][j]+=arr1[i][k]*arr2[k][j];
+             }
         }
     }
     return answer;
