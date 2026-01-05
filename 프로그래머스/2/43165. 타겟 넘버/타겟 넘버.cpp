006934416b8922766805e75d1aa78617f3eddf2a
PDY <164182578+pdy127001@users.noreply.github.com>
2026-01-05 09:24:32 +0900

    [level 2] Title: 타겟 넘버, Time: 3.70 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(const vector<int>& numbers, int target, int idx, int sum)
+{
+    if (idx == numbers.size())
+    {
+        if (sum == target){
+            answer++;
+        }
+        return;
+    }
+    dfs(numbers, target, idx + 1, sum + numbers[idx]);
+    dfs(numbers, target, idx + 1, sum - numbers[idx]);
+}
+
+int solution(vector<int> numbers, int target)
+{
+    answer = 0;
+    dfs(numbers, target, 0, 0);
+    return answer;
+}
